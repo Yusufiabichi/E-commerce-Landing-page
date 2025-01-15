40a7a239870cd5b5988c54cfe65a66422ae6314a
Yusufia <Yusufiabichi@yahoo.com>
2025-01-15 13:28:18 -0800

    Nav links

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,28 @@
 
 const Hero = () => {
   return (
-    <div>Test</div>
+    <section id="home"
+    className="w-full flex xl:flex-row flex-col
+    justify-center min-h-screen
+    gap-10 max-container 
+    border-2 p-2 border-red-500"
+    >
+      <div className="relative xl:w-2/5 flex-col 
+      justify-center items-start w-full max-xl:padding-x 
+      pt-28
+      ">
+        <p>Our Summar Collection</p>
+        <h1>
+          <span>The New Arrivals</span>
+          <br />
+          <span>Nike</span>
+        </h1>
+          <p>Discover stylish Nike arrivals, quality comfort,
+            and innovation for your active life.
+          </p>
+      </div>
+
+    </section>
   )
 }
 
