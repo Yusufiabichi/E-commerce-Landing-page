be76ad03c3a1a38b31d191cd650a2d326b8d04c1
Yusufia <Yusufiabichi@yahoo.com>
2025-01-15 12:51:18 -0800

    tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Tailwind CSS file
-import Header from './layout/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
