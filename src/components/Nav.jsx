d245f225dbd35d177824694af1b62c0c8ef84beb
Yusufia <Yusufiabichi@yahoo.com>
2025-01-15 13:13:34 -0800

    Nav links

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,12 +14,12 @@ const Nav = () => {
           />
         </a>
         <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-          {navLinks.map((ite)=> (
+          {navLinks.map((item)=> (
             <li key={item.label}>
               <a href={item.href}
               className='font-montserrat leading-normal text-lg text-slate-gray'
               >
-                
+                {item.label}
               </a>
             </li>
           ))}
