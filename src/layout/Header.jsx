3a9ad096534ceeae470b2a0609790e8036d03a72
Yusufia <Yusufiabichi@yahoo.com>
2025-01-15 02:38:39 -0800

    header component

---
+++ src/layout/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <>
+            <div className="p-10 bg-blue-500 text-white text-center">
+                
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                    <li>Contact</li>
+            </div>
+        </>
+    );
+  }
+  
+  export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // All JS and TS files in the src directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
