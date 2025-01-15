904a6ab8cca62a6cff46f14d103274f1f5cd680a
Yusufia <Yusufiabichi@yahoo.com>
2025-01-15 08:56:02 -0800

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-// export default function App() {
-//   return (
-//     <h1 class="font-bold underline text-3xl">
-//       Hello React World
-//     </h1>
-//   )
-// }
+import { CustomerReviews, Footer, Nav, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
+const App = () => (
+  <main className="relative">
+    <Nav />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding bg-black paddind-x padding-t pb-8 text-white">
+      <Footer />
+    </section>
+  </main>
+)
 
-
-// function App() {
-//   return (
-//     <div className="flex items-center justify-center underline h-screen bg-gray-100">
-//       <h1 className="text-4xl font-bold text-blue-500">
-//         Hello, Tailwind CSS with Vite!
-//       </h1>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-function App() {
-  return (
-    <div className="p-10 bg-blue-500 text-white text-center">
-      Tailwind CSS is working!
-    </div>
-  );
-}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import Header from './layout/Header'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header />
+    
     <App />
   </React.StrictMode>
 );
