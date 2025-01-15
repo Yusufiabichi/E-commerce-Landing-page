4d34fdf4825b88afd9ecbc99052e22971597cffd
Yusufia <Yusufiabichi@yahoo.com>
2025-01-15 09:02:08 -0800

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { CustomerReviews, Footer, Nav, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
+import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from "./sections";
+import Nav from "./components/Nav";
 const App = () => (
   <main className="relative">
     <Nav />
