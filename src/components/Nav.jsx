93ce5121960973bf587575cc3f08cd7376e074b4
Yusufia <Yusufiabichi@yahoo.com>
2025-01-15 12:56:12 -0800

    tailwind

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -13,7 +13,7 @@ const Nav = () => {
           />
         </a>
         <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-
+            
         </ul>
       </nav>
     </header>
