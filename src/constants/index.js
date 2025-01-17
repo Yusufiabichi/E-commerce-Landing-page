48030af43abe564538e9564770557eb6557c4631
Yusufia <Yusufiabichi@yahoo.com>
2025-01-17 01:30:18 -0800

    Popular prod

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
 import { bigShoe1, bigShoe2, bigShoe3, customer1, customer2, shoe4, shoe5, shoe6, 
-    shoe7, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3 } from "../assets/images";
+    shoe7, car4, car5, car6, car7, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3 } from "../assets/images";
 
 export const navLinks = [
     { href: "#home", label: "Home" },
