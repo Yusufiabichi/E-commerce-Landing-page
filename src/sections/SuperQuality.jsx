d7ca55b77f876713fa51b05831d66c292c1ba6ff
Yusufia <Yusufiabichi@yahoo.com>
2025-01-17 03:25:25 -0800

    Super quality

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,6 @@
 import Button from '../components/Button'  
 import { arrowRight } from '../assets/icons'
+import { shoe8 } from '../assets/images'
 const SuperQuality = () => {
   return (
     <section id="about-us" className="flex justify-between
@@ -9,7 +10,6 @@ const SuperQuality = () => {
         <h2 className='font-palanquin text-4xl capitalize font-bold
         lg:max-w-lg'>
           We Provide you
-          <br />
           <span className='text-coral-red'> Super </span>
           <span className='text-coral-red'>Quality </span> Cars
         </h2>
@@ -27,6 +27,14 @@ const SuperQuality = () => {
 
           </div>
 
+      </div>
+
+      <div className='flex-1 flex justify-center items-center'>
+        <img src={shoe8} 
+        alt="shoe image"
+        width={570}
+        height={522}
+        className='object-contain' />
       </div>
     </section>
   )
