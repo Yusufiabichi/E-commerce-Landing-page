71cc387cf703c29e0c7bb1fa1ff2b0f8907f46c8
Yusufia <Yusufiabichi@yahoo.com>
2025-01-17 23:41:10 -0800

    Special offer

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,22 @@
+
+
+
+const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
+  return (
+    <div className="flex justify-center items-center flex-col">
+        <img src={imgURL} 
+        alt="customer"
+        className="rounded-full object-cover w-[120px h-[120px]" />
+        <p>{feedback}</p>
+        <div>
+            <img src={star} 
+            alt="star"
+            width={24}
+            height={24}
+            className="" />
+        </div>
+    </div>
+  )
+}
+
+export default ReviewCard
