ca603221ff0150c8a63dee068f51730e7638cccd
Yusufia <Yusufiabichi@yahoo.com>
2025-01-18 04:01:02 -0800

    subscribe

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -11,8 +11,8 @@ const Subscribe = () => {
         </span> & Newsletters
       </h3>
 
-      <div className="lg-max-w-[40%] w-full flex items-center max-sm:flex-col gap-5p-2.5
-      sm:border sm:border-slate-gray">
+      <div className="lg-max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5
+      sm:border sm:border-slate-gray rounded-full">
         <input type="text"
         placeholder="Enter Your Email to subscribe"
         className="input" />
