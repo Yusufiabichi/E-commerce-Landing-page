4049a59b1c07667b8e6621754a3e2d249c1a8ab6
Yusufia <Yusufiabichi@yahoo.com>
2025-01-18 04:34:01 -0800

    footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,4 @@
+import { copyrightSign } from "../assets/icons"
 import { footerLogo } from "../assets/images"
 import { footerLinks, socialMedia } from "../constants"
 
@@ -49,6 +50,20 @@ const Footer = () => {
                 </ul>
               </div>
             ))}
+        </div>
+      </div>
+
+      <div className="fleex justify-between text-white-400
+      mt-24 max-sm:flex-col max-sm:items-center">
+
+        <div className="flex flex-1 justify-start items-center gap-2 font-montserrat
+        cursor-pointer">
+          <img src={copyrightSign} 
+          alt="copy right sign"
+          width={20}
+          height={20}
+          className="rounded-full m-0" />
+          <p>Copyright. All right Reserved.</p>
         </div>
       </div>
     </footer>
