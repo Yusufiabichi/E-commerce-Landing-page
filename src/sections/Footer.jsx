dab06e5097ce43607a0caa5b0a622b436400125c
Yusufia <Yusufiabichi@yahoo.com>
2025-01-18 04:09:44 -0800

    footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -7,8 +7,14 @@ const Footer = () => {
       flex-wrap max-lg:flex-col">
         <div className="flex flex-col items-start">
           <a href="/">
-            <img src={footerLogo} alt="" />
+            <img src={footerLogo} 
+            alt="footer logo"
+            width={150}
+            height={46} />
           </a>
+          <p>Get Shoes Ready for the new term at your nearest Nike store.
+            Find your perfect size in store. Get rewards
+          </p>
         </div>
       </div>
     </footer>
