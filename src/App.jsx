4e45de4456c5ca933be1834ffc2206cfd66b1e9f
Yusufia <Yusufiabichi@yahoo.com>
2025-01-21 14:13:54 -0800

    slider

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ const App = () => (
   <main className="relative">
     <Nav />
     <section className="xl:padding-l wide:padding-r padding-b">
+      <Slider />
+    </section>
+    <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
     <section className="padding">
