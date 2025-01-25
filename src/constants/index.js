f0c2bc23fcc312ffdd2e1fa9427c27022bcd4704
Yusufia <Yusufiabichi@yahoo.com>
2025-01-25 00:04:42 -0800

    cng

--- src/constants/index.js
+++ src/constants/index.js
@@ -107,18 +107,18 @@ export const footerLinks = [
     {
         title: "Products",
         links: [
-            { name: "Air Force 1", link: "/" },
-            { name: "Air Max 1", link: "/" },
-            { name: "Air Jordan 1", link: "/" },
-            { name: "Air Force 2", link: "/" },
-            { name: "Nike Waffle Racer", link: "/" },
-            { name: "Nike Cortez", link: "/" },
+            { name: "BMW X Series", link: "/" },
+            { name: "Marcedes Benz", link: "/" },
+            { name: "Audi S seiries", link: "/" },
+            { name: "Toyota Hilux Brands", link: "/" },
+            { name: "Lexus brands", link: "/" },
+            { name: "Tesla and others", link: "/" },
         ],
     },
     {
         title: "Help",
         links: [
-            { name: "About us", link: "/" },
+            { name: "About us", link: "/#home" },
             { name: "FAQs", link: "/" },
             { name: "How it works", link: "/" },
             { name: "Privacy policy", link: "/" },
@@ -128,8 +128,8 @@ export const footerLinks = [
     {
         title: "Get in touch",
         links: [
-            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "customer@mangamotors.com", link: "mailto:yusufiadesigns@gmail.com" },
+            { name: "+2347068538000", link: "tel:+2347068538000" },
         ],
     },
 ];
